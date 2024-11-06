e914e1347539d67ac3c97f09e168d95fa2727c80
Ole Rohne <ole.rohne@cern.ch>
2024-11-06 04:19:44 +0100

    Fixed main -> master modified:   rebase.sh

--- rebase.sh
+++ rebase.sh
@@ -1,7 +1,7 @@
 git stash
 git checkout master
 git pull --rebase origin
-git branch | grep / | while read b; do echo Rebasing $b; git rebase main $b; done
+git branch | grep / | while read b; do echo Rebasing $b; git rebase master $b; done
 git branch -D merged.old
 git branch -M merged merged.old
 git checkout -b merged master
